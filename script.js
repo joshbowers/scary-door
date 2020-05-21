9b48a6c1c58f45b1db88bca22c393853c806bf7a
Joshua Bowers <52871816+joshbowers@users.noreply.github.com>
2020-05-21 12:12:16 -0500

    Add files via upload
    Initial upload with HTML, CSS, and JS.

---
+++ script.js
@@ -0,0 +1,73 @@
+var input = document.getElementById("doorinput");
+var button = document.getElementById("enterbutton")
+
+function inputValue() {
+	return input.value;
+}
+
+function inputLength() {
+	return input.value.length;
+}
+
+function openDoorAfterClick() {
+	if (inputLength() > 0) {
+		openDoor();
+	}
+}
+function openDoorAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		openDoor();
+	}
+}
+
+// function openPage() {
+//     alert("Which door do you choose to open... One, two, three, or four?")
+//     alert("Enter your choice below... if you dare!!!1!1!")
+// }
+
+function openDoor() {
+    var whatsBehindDoor;
+    switch (inputValue()) {
+        // Open door one
+        case "one":
+            whatsBehindDoor = "A breifcase with 1 trillion dollars...!" 
+            + " that causes your limbs to fall of when you spend it.";
+            break;
+        case "1":
+            whatsBehindDoor = "A breifcase with 1 trillion dollars...!" 
+            + " that causes your limbs to fall of when you spend it.";
+            break;
+        // Open door two
+        case "two":
+            whatsBehindDoor = "The person of your dreams...! "
+            + " but they sell Mary Kay products.";
+            break;
+		case "2":
+            whatsBehindDoor = "The person of your dreams...! "
+            + " but they sell Mary Kay products.";
+            break;
+        // Open door three
+        case "three":
+            whatsBehindDoor = "The gift of flight...! but you can only "
+            + " fly three feet off of the ground.";
+            break;
+        case "3":
+            whatsBehindDoor = "The gift of flight...! but you can only "
+            + " fly three feet off of the ground.";
+            break;
+        // Open door three
+        case "four":
+            whatsBehindDoor = "The gift of acceptance...!"
+            break;
+        case "4":
+            whatsBehindDoor = "The gift of acceptance...!"
+            break;
+        default:
+            whatsBehindDoor = "Which door do you choose to open... One, two, three, or four?"
+    }
+    return alert(whatsBehindDoor);
+}
+
+// addEventListener("load", openPage);
+input.addEventListener("keypress", openDoorAfterKeypress);
+button.addEventListener("click", openDoorAfterClick);
